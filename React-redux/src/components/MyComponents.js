b987831baa9a98c940214e32a7db191997144641
Thenam2kx <thenam2kx@gmail.com>
2024-06-26 13:25:07 +0700

    Lesson 21: Form

--- React-redux/src/components/MyComponents.js
+++ React-redux/src/components/MyComponents.js
@@ -18,11 +18,33 @@ class MyComponents  extends React.Component {
   }
 
 
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
         <p>my name is {this.state.name}</p>
         <button onClick={ (event) => {this.handleClick(event)} }>Click me</button>
+      
+        <pre/>
+
+        <form onSubmit={ (event) => this.handleOnSubmit(event) }>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={ (event) => {this.handleOnChange(event)} }  
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
